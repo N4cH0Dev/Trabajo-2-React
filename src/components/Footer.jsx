be50950e52e43b7d141bedbee9ca75f2a7c9ce39
N4cH0Dev <joseignaciodiazromero.dev@gmail.com>
2025-05-06 01:50:34 -0300

    se agrego clase a footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,8 @@
 import React from "react";
-
+import "../css/Style.css";
 function Footer() {
   return (
-    <footer
-      style={{
-        backgroundImage:
-          "url('https://codersfree.nyc3.cdn.digitaloceanspaces.com/posts/conoce-5-lenguajes-de-programacion-basicos.jpg')",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        padding: "20px",
-      }}
-    >
+    <footer>
       <h2 style={{ color: "blue" }}>FRT UTN 2024</h2>
       <p style={{ color: "red" }}>
         La Facultad Regional Tucumán es una unidad académica dependiente de la
