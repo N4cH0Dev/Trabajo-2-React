a95a38b216b986bf252485849527d4ffe8991fe5
N4cH0Dev <joseignaciodiazromero.dev@gmail.com>
2025-05-06 00:58:24 -0300

    Se creo la pagina de nosotros

--- src/pages/Nosotros.jsx
+++ src/pages/Nosotros.jsx
@@ -1,31 +1,36 @@
-// src/pages/Nosotros.jsx
-import React from "react";
-import LangAlumnos from "../data/LangAlumnos"; // Asegúrate de que la ruta sea correcta
 import "../css/Style.css";
-
+import nacho from "../images/nacho.jpeg";
+import nico from "../images/nico.jpeg";
 function Nosotros() {
+  const datos = [
+    {
+      id: 1,
+      nombre: "Jose Ignacio Diaz Romero",
+      descripcion:
+        "Soy estudiante de la tecnicatura en programacion, con experiencia previa en el desarrollo de sistemas web utilizando tecnologías como MySQL y GeneXus. Me destaco por mi enfoque en la arquitectura lógica de los proyectos, la documentación clara y la integración entre frontend y backend. En este trabajo me encargué del armado de la estructura del proyecto, diseño de componentes reutilizables y conexión con estilos globales.",
+      imagen: nacho,
+    },
+    {
+      id: 2,
+      nombre: "Nicolas Fernandez",
+      descripcion:
+        "Soy estudiante de programación y me estoy formando en desarrollo frontend. Me apasiona crear interfaces limpias y funcionales. Estoy aprendiendo React y tengo conocimientos de HTML, CSS y JavaScript. En este proyecto trabajé principalmente en el diseño visual y la adaptación responsiva, asegurándome de que la aplicación sea clara y fácil de usar en diferentes dispositivos.",
+      imagen: nico,
+    },
+  ];
+
   return (
-    <div className="nosotros">
-      <h2>Conocé a nuestros alumnos</h2>
-      <div className="alumnos-container">
-        {LangAlumnos.map((alumno) => (
-          <div className="alumno-card" key={alumno.id}>
-            <h3>{alumno.nombre}</h3>
-            <p>
-              <strong>Edad:</strong> {alumno.edad} años
-            </p>
-            {alumno.imagen ? (
-              <img
-                src={alumno.imagen}
-                alt={alumno.nombre}
-                className="foto-alumno"
-              />
-            ) : (
-              <div className="sin-imagen">Sin imagen disponible</div>
-            )}
+    <div className="nosotros-container">
+      <h1>Sobre Nosotros</h1>
+      {datos.map((persona) => (
+        <div className="card-nosotros" key={persona.id}>
+          <img src={persona.imagen} alt={`Foto de ${persona.nombre}`} />
+          <div className="info">
+            <h2>{persona.nombre}</h2>
+            <p>{persona.descripcion}</p>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
   );
 }
